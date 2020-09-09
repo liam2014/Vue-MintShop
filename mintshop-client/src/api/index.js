5e86a5d81f3b6d7289363b1d6b57aa9f735ea5ba
liamA <37907909@qq.com>
2020-09-09 16:12:48 +0800

    feat: api 切换；

--- mintshop-client/src/api/index.js
+++ mintshop-client/src/api/index.js
@@ -2,47 +2,47 @@
 与后台交互模块 （依赖已封装的ajax函数）
  */
 import ajax from './axios' // './ajax' //
-// const BASE_URL = 'http://local:4000'
-const BASE_URL = '/api'
+const BASE_URL = 'http://localhost:4001'
+// const BASE_URL = '/api'
 /**
  * 获取地址信息(根据经纬度串)
  * 这个接口的经纬度参数是在url路径里的，没有query参数
  */
-export const reqAddress = geohash => ajax(`${BASE_URL}/position/${geohash}`)
+export const reqAddress = geohash => ajax(`${BASE_URL}/api_app/api/v1/shop/position?location=${geohash}`)
 /**
  * 获取 msite 页面食品分类列表
  */
-export const reqCategorys = () => ajax(BASE_URL + '/index_category')
+export const reqCategorys = () => ajax(BASE_URL + '/api_app/api/v1/shop/index-category')
 /**
  * 获取 msite 商铺列表(根据query参数：经纬度)
  * 将经纬度两个数据作为一个参数对象传入
  * 也可以两个数据分别传入ajax， 然后再放入一个对象参数内， 如下面的手机号接口
  */
-export const reqShops = ({latitude, longitude}) => ajax(BASE_URL + '/shops', {latitude, longitude})
+export const reqShops = ({latitude, longitude}) => ajax(BASE_URL + '/api_app/api/v1/shop/shops', {latitude, longitude})
 /**
  * 根据经纬度和关键字搜索商铺列表
  */
-export const reqSearchShop = (geohash, keyword) => ajax(BASE_URL + '/search_shops', {geohash, keyword})
+export const reqSearchShop = (geohash, keyword) => ajax(BASE_URL + '/api_app/api/v1/shop/search-shops', {geohash, keyword})
 /**
  * 账号密码登录
  */
-export const reqPwdLogin = ({name, pwd, captcha}) => ajax(BASE_URL + '/login_pwd', {name, pwd, captcha}, 'POST')
+export const reqPwdLogin = ({name, pwd, captcha}) => ajax(BASE_URL + '/api_app/api/v1/user/login-pwd', {name, pwd, captcha}, 'POST')
 /**
  * 获取短信验证码
  */
-export const reqSendCode = phone => ajax(BASE_URL + '/sendcode', {phone})
+export const reqSendCode = phone => ajax(BASE_URL + '/api_app/api/v1/user/login-sms', {phone})
 /**
  * 手机号验证码登录
  */
-export const reqSmsLogin = (phone, code) => ajax(BASE_URL + '/login_sms', {phone, code}, 'POST')
+export const reqSmsLogin = (phone, code) => ajax(BASE_URL + '/api_app/api/v1/user/login-sms', {phone, code}, 'POST')
 /**
  * 获取用户信息(根据会话)
  */
-export const reqUserInfo = () => ajax(BASE_URL + '/userinfo')
+export const reqUserInfo = () => ajax(BASE_URL + '/api_app/api/v1/user/info')
 /**
  * 请求登出
  */
-export const reqLogout = () => ajax(BASE_URL + '/logout')
+export const reqLogout = () => ajax(BASE_URL + '/api_app/api/v1/user/logout')
 /**
  * 获取商家信息(下列请求由mock拦截并返回 不需要代理)
  */
